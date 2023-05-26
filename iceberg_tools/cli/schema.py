9e9b667fccdfbfafac48e4515331bf53a4f0d4f5
Brian Walsh <brian@bwalsh.com>
2023-05-26 08:41:55 -0700

    Removes schema publish command

--- iceberg_tools/cli/schema.py
+++ iceberg_tools/cli/schema.py
@@ -148,24 +148,3 @@ def compile_gen3(output_path):
     output_path = pathlib.Path(output_path)
     compile_gen3_schemas(output_path)
     logger.info(f'Gen3 schemas written to {output_path / pathlib.Path("aced.json")}')
-
-
-@cli.command(name='publish')
-@click.argument('dictionary_path', default='iceberg/schemas/gen3/aced.json')
-@click.option('--bucket', default="s3://aced-public", help="Bucket target", show_default=True)
-@click.option('--production', default=False, is_flag=True, show_default=True,
-              help="Write to aced.json, otherwise aced-test.json")
-def schema_publish(dictionary_path, bucket, production):
-    """Copy dictionary to s3 (note:aws cli dependency)"""
-
-    dictionary_path = pathlib.Path(dictionary_path)
-    assert dictionary_path.is_file(), f"{dictionary_path} should be a path"
-    click.echo(f"Writing schema into {bucket}")
-    import subprocess
-    if production:
-        cmd = f"aws s3 cp {dictionary_path} {bucket}".split(' ')
-    else:
-        cmd = f"aws s3 cp {dictionary_path} {bucket}/aced-test.json".split(' ')
-    s3_cp = subprocess.run(cmd)
-    assert s3_cp.returncode == 0, s3_cp
-    print("OK")
