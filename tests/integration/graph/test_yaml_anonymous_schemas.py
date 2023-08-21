dd840b1339a725b50238c6561f4538b1c1835d66
Brian Walsh <brian@bwalsh.com>
2023-08-21 11:59:16 -0700

    Ensure extra links not written to schema.description

--- tests/integration/graph/test_yaml_anonymous_schemas.py
+++ tests/integration/graph/test_yaml_anonymous_schemas.py
@@ -7,4 +7,10 @@ def test_yaml_anonymous_schemas(bmeg_dir):
     """Check individual anonymous schemas"""
     for file_name in pathlib.Path('').glob("*.yaml"):
         with open(file_name) as fp:
-            Draft202012Validator.check_schema(yaml.load(fp, yaml.SafeLoader))
+            schema = yaml.load(fp, yaml.SafeLoader)
+            Draft202012Validator.check_schema(schema)
+            description = schema.get('description', None)
+            assert description is not None, f"Missing description in {file_name}"
+            assert '[See' in description, f"Missing [See ...] in {file_name}"
+            # test to ensure See only appears once
+            assert description.count('[See') == 1, f"Multiple [See ...] in {file_name}"
