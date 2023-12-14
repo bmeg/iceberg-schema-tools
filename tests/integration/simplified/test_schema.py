5b0918ba9dfd12bd6d0807cc2d3359410b7cf57b
Brian Walsh <brian@bwalsh.com>
2023-12-14 06:42:34 -0800

    Adds test for dictionaryutils.DataDictionary

--- tests/integration/simplified/test_schema.py
+++ tests/integration/simplified/test_schema.py
@@ -1,7 +1,13 @@
 import fastjsonschema
+from iceberg_tools.schema.simplified_validator import ensure_schema
 
 
 def test_schema(distribution_schema):
     """Ensure the schema compiles as a legal json schema."""
     compiled_schema = fastjsonschema.compile(distribution_schema)
     assert compiled_schema
+
+
+def test_schema_load(distribution_schema_path):
+    """Ensure we can load using dictionaryutils."""
+    assert ensure_schema(schema_path=distribution_schema_path), f"should have loaded {distribution_schema_path}"
