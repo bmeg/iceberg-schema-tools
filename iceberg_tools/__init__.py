688329b62ba93d6b01d879563ecc96360751ac61
matthewpeterkort <matthewpeterkort@gmail.com>
2023-12-15 12:21:13 -0800

    tests pass but schemas are different

--- iceberg_tools/__init__.py
+++ iceberg_tools/__init__.py
@@ -4,13 +4,13 @@ from typing import Union
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 def monkey_patch_url_validate():
     # monkey patch to allow file: urls
     import fhir.resources.fhirtypes
     from pydantic.v1 import FileUrl
 
     original_url_validate = fhir.resources.fhirtypes.Url.validate
-
 
     @classmethod
     def better_url_validate(  # type: ignore
@@ -24,8 +24,8 @@ def monkey_patch_url_validate():
         value = original_url_validate(value, field, config)
         return value
 
-
     fhir.resources.fhirtypes.Url.validate = better_url_validate
     logger.debug("monkey patched Url.validate")
+
 
 monkey_patch_url_validate()
